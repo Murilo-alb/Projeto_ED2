9788c226496a21ac73c4eee70d7d16ccee3e830e
Murilo-alb <albuquerquemurilo10@gmail.com>
2024-11-06 19:03:16 -0300

    Update Projeto_Final.c

--- ATIVIDADE_1.c
+++ ATIVIDADE_1.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,51 +13,52 @@ typedef struct paciente
 	
 }Tpaciente;
 
+Tpaciente *alocarNo(int ID, char *nome, int idade, char *condicao_medica);
+Tpaciente *buscarID(Tpaciente *raiz, int ID); //Busca codigo pelo ID;
+Tpaciente *buscarNome(Tpaciente *raiz, char *nome); //Busca codigo pelo nome;
+Tpaciente **buscaPaiID(Tpaciente **raiz, int k); //Acha nó anterior pelo Id;
+Tpaciente **buscaPaiNome(Tpaciente **raiz, char *nome); //Acha no anterior pelo nome;
+Tpaciente **ppMenor(Tpaciente **raiz);//Achar o menor nó;
 
-Tpaciente *alocarNo(int ID, char *nome, int idade, char *condicao_medica);//criar um n� na memoria
-Tpaciente *buscar_PacienteID(Tpaciente *raiz, int ID);
-Tpaciente *buscar_PacienteNome(Tpaciente *raiz, char *nome);
-Tpaciente **buscaPaiID(Tpaciente **raiz, int k);
-Tpaciente *buscaPaiNome(Tpaciente *raiz, char *nome);
-Tpaciente **ppMenor(Tpaciente **raiz);
-
-
-void insereID(Tpaciente **raiz, int ID, char *nome, int idade, char *condicao_medica); //Arvore por ID
-void insereNome(Tpaciente **raiz, int ID, char *nome, int idade, char *condicao_medica); // Arvore por NOME
-void imprimeID(Tpaciente *raiz);
+void insere(Tpaciente **raiz, int ID, char *nome, int idade, char *condicao_medica); 
+void imprimeID(Tpaciente *raiz); 
 void imprimeNome(Tpaciente *raiz);
-void removerID(Tpaciente **raiz, int ID);
-void removerNome(Tpaciente **raiz, char *nome);
+void removerID(Tpaciente **raiz, int ID); //Remove o usuario pelo ID;
+void removerNome(Tpaciente **raiz, char *nome);//Remove o usuario pelo nome;
+void salvar(Tpaciente *raiz, FILE *arq); //Salva o arquivo;
+
 
 int main(){
+	
+	//Processo de leitura de um arquivo
 		FILE *arq=NULL;
-		arq=fopen("paciente1.txt","r");
-		if(arq == NULL)
+		arq=fopen("paciente1.txt","r"); //Abrindo o arquivo para leitura
+		if(arq == NULL){
+		printf("Erro");	
 			return 1;
-			
-						
-		Tpaciente *raiz_ID = NULL, *raiz_Nome = NULL, *achouID = NULL, *achouNome = NULL;
+		}
+		
+		Tpaciente *raiz_ID = NULL, *achou = NULL; // achou serve para buscar o ID ou Nome;
 		int ID, idade, aux = 0;
 		char nome[20];
 		char condicao_medica[30];
+		int op = 1, contador = 0;
 		
-		int op = 1;
-		while(!feof(arq)){
-	
-			fscanf(arq, "%d %s %d %s", &ID, nome, &idade, condicao_medica);
-			
-			 insereID(&raiz_ID, ID, nome, idade, condicao_medica);
-				insereNome(&raiz_Nome, ID, nome, idade, condicao_medica);
-					
-		
+		//guardando nas variaveis;
+		while (fscanf(arq, "%d %s %d %s", &ID, nome, &idade, condicao_medica) == 4){
+			 insere(&raiz_ID, ID, nome, idade, condicao_medica);
+			 contador++;
 		}
 			fclose(arq);
-			printf("Raiz: %d\n", raiz_ID->ID);
 			
-			while(op != 7){
-				printf("1 - Imprimir por ID\n2 - Imprimeir por Nome\n3 - Buscar por ID\n4 - Buscar por Nome\n5 - Remover ID\n:");
+			
+			printf("Raiz: %d\n", raiz_ID->ID); //Imprime o nó raiz da arvoré
+			
+			while(op != 8){
+				printf("1 - Imprimir por ID\n2 - Imprimeir por Nome\n3 - Cadastrar Paciente \n4 - Buscar por ID \n5 - Buscar por Nome\n6 - Remover ID\n7 - Remover Nome \n:");
 					scanf("%d", &op);
-				fflush(stdin);
+				fflush(stdin); 
+				
 				switch(op){	
 				 	case 1:{
 				 		printf("\n\n");
@@ -69,44 +69,76 @@ int main(){
 					
 					case 2:{
 						printf("\n");
-						imprimeNome(raiz_Nome);
+						imprimeNome(raiz_ID);
 						printf("\n");
 							
 					}break;
 					
-					case 3:{
-						printf("Informe o ID: ");
-							scanf("%d", &aux);
-						achouID = buscar_PacienteID(raiz_ID, aux);
-						
-						if(achouID)
-							printf("\nID encontrado: %d\n", achouID->ID);
-						else
-							printf("\nN�o existe\n");
-							
+					case 3: {
+						printf("\nInforme ID, Nome, Idade e Condição medica do novo paciente\n: ");
+						scanf("%d %s %d %s", &ID, nome, &idade, &condicao_medica);
+						insere(&raiz_ID, ID, nome, idade, condicao_medica);
 						
 					}break;
 					
 					case 4:{
-						char aux_nome[20];
-						printf("Informe o Nome Completo: ");
-							scanf("%s", aux_nome);
-						achouNome = buscar_PacienteNome(raiz_Nome, aux_nome);
-						
-						if(achouNome)
-							printf("\nNome encontrado: %s\n\n", achouNome->nome);
-						else
-							printf("\nNome nao existe\n");	
-						
+						printf("Informe o ID: ");
+							scanf("%d", &aux);
+						achou = buscarID(raiz_ID, aux); //Utilização do achou por ID;
+						if(achou){
+							printf("\nID encontrado: %d\n", achou->ID);
+						 printf("\nId: %d - %s - Idade: %d - %s\n\n", achou->ID, achou->nome, achou->idade, achou->condicao_medica);
+					
+						}else
+							printf("\nNão existe\n");		
 					}break;
 					
 					case 5:{
-						printf("\nInforme o valor desejado: ");
-							scanf("%d", &aux);
-							removerID(&raiz_ID, aux);	
+						char aux_nome[20];
+						printf("Informe o Nome Completo: ");
+							scanf("%s", aux_nome);
+							
+						achou = buscarNome(raiz_ID, aux_nome); //Utilização do achou por Nome;
+						
+						if(achou){
+							printf("\nNome encontrado: %s\n\n", achou->nome);
+							printf("\n%s - Id: %d - Idade: %d - %s\n\n", achou->nome, achou->ID, achou->idade, achou->condicao_medica);
+					
+						}else
+							printf("\nNome nao existe\n");	
 					}break;
+					
+					case 6: {
+                printf("Informe o ID do paciente para remover: ");
+                scanf("%d", &ID);
+                removerID(&raiz_ID, ID);
+                contador--; //Contador serve para a contagem dos pacientes;
+            }break;
+
+            case 7: {
+                printf("Informe o Nome do paciente para remover: ");
+                scanf("%s", nome);
+                removerNome(&raiz_ID, nome);
+                contador--;
+            }break;
+            
+            case 8: {
+            	printf("\nSaiu..");
+            	printf("Total Pacientes: %d", contador);
+            		
+				arq=fopen("paciente1.txt","w"); //Abrindo o arquivo para escrita
+            	if(arq == NULL){
+            		return 1;
+				}
+					salvar(raiz_ID, arq);
+					fclose;
+				printf("\n\nFoi salvo");
+			}break;
+					default: 
+						printf("Numero invalido");
 				}
 		}
+		return 0;
 }
 
 Tpaciente *alocarNo (int ID, char *nome, int idade, char *condicao_medica)
@@ -115,10 +147,11 @@ Tpaciente *alocarNo (int ID, char *nome, int idade, char *condicao_medica)
     novo = (Tpaciente *)malloc(sizeof(Tpaciente));
     if(novo)
     {
+    	//Atribuindo os valores
        	novo->ID = ID;
-       	strcpy(novo->nome, nome);
+       	strcpy(novo->nome, nome); //strcpy para copiar uma string;
        	novo->idade = idade;
-       strcpy(novo->condicao_medica, condicao_medica);
+       	strcpy(novo->condicao_medica, condicao_medica); 
         novo->dir = NULL;
         novo->esq = NULL;
         
@@ -127,35 +160,32 @@ Tpaciente *alocarNo (int ID, char *nome, int idade, char *condicao_medica)
     return novo;
 }
 
-void insereID(Tpaciente **raiz, int ID, char *nome, int idade, char *condicao_medica)
+void insere(Tpaciente **raiz, int ID, char *nome, int idade, char *condicao_medica)
 {
     if(*raiz == NULL)
         *raiz = alocarNo(ID, nome, idade, condicao_medica);
     else{
-	
-        if(ID < (*raiz)->ID)
-            insereID(&(*raiz)->esq, ID, nome, idade, condicao_medica);
-    	else
-            insereID(&(*raiz)->dir, ID, nome, idade, condicao_medica);
+    	//Confere se ja existe um ID;
+		if (buscarID(*raiz, ID) != NULL) {
+       		printf("ID já existe!\n");
+        	return;
+    }	
+    	//Confere se ja existe um Nome;
+   		 if (buscarNome(*raiz, nome) != NULL) {
+       		printf(" Nome ja existe!\n");
+       		 return;
+    	}
+    	//Recursividade;
+    	if (ID < (*raiz)->ID)
+        	insere(&(*raiz)->esq, ID, nome, idade, condicao_medica);
+        else
+            insere(&(*raiz)->dir, ID, nome, idade, condicao_medica);
 	}
-    
-}
-
-
-void insereNome(Tpaciente **raiz, int ID, char *nome, int idade, char *condicao_medica)
-{
-	if(*raiz == NULL)
-		*raiz = alocarNo(ID, nome, idade, condicao_medica);
-		else{
-		if(strcmp(nome, (*raiz)->nome) < 0)
-    		 insereNome(&(*raiz)->esq, ID, nome, idade, condicao_medica);
-    	else
-           	 insereNome(&(*raiz)->dir, ID, nome, idade, condicao_medica);
-		}
 }
 
 void imprimeID(Tpaciente *raiz)
 {
+	//Imprime in-Order
 	  if (raiz != NULL) {
         imprimeID(raiz->esq);
         printf("%d - ", raiz->ID);
@@ -165,19 +195,16 @@ void imprimeID(Tpaciente *raiz)
 
 void imprimeNome(Tpaciente *raiz)
 {
-
 	if(raiz != NULL)
-	{
+	{	//Imprime in-Order pela tabela ascii
 		imprimeNome(raiz->esq);
-		printf("%s", raiz->nome);
-		printf("\n");
+		printf("%s\n", raiz->nome);
 		imprimeNome(raiz->dir);
-		
 	}
 
 }
 
-Tpaciente *buscar_PacienteID(Tpaciente *raiz, int ID)
+Tpaciente *buscarID(Tpaciente *raiz, int ID)
 {
     if(raiz == NULL)
         return NULL;
@@ -187,31 +214,32 @@ Tpaciente *buscar_PacienteID(Tpaciente *raiz, int ID)
     else
     {
         if(ID < raiz->ID)
-            return buscar_PacienteID(raiz->esq, ID);
+            return buscarID(raiz->esq, ID);
         else
-            return buscar_PacienteID(raiz->dir, ID);
+            return buscarID(raiz->dir, ID);
     }
 }
 
-Tpaciente *buscar_PacienteNome(Tpaciente *raiz, char *nome)
+Tpaciente *buscarNome(Tpaciente *raiz, char *nome)
 {
 	if(raiz == NULL)
 		return NULL;
-	int comparar = strcmp(nome, raiz->nome);
+	int comparar = strcmp(nome, raiz->nome); // compara duas strings
 	
 	if(comparar == 0)
 		return raiz;
 		
 	if(comparar < 0)
-		return buscar_PacienteNome(raiz->esq, nome);
+		return buscarNome(raiz->esq, nome);
 	else
-		return buscar_PacienteNome(raiz->dir, nome);		
+		return buscarNome(raiz->dir, nome);		
 }
 
 Tpaciente **buscaPaiID(Tpaciente **raiz, int ID)
 {
 	if (*raiz == NULL)
     return NULL;
+    
   while(*raiz!=NULL)
   {
       if((*raiz)->ID == ID)
@@ -224,139 +252,128 @@ Tpaciente **buscaPaiID(Tpaciente **raiz, int ID)
   }
   
 }
-Tpaciente *buscaPaiNome(Tpaciente *raiz, char *nome)
+Tpaciente **buscaPaiNome(Tpaciente **raiz, char *nome)
 {
-	int comparar = strcmp(nome, raiz->nome);
-	if (raiz == NULL)
-    	return NULL;
-    	
-  while(raiz!=NULL)
-  {
-      if(comparar == 0)
-        return raiz;
-        
-      if(comparar < 0)
-        return buscaPaiNome(raiz->esq, nome);
-      else
-        return buscaPaiNome(raiz->dir, nome);
-        
-	}	
+    while (*raiz != NULL) {
+        int comparar = strcmp(nome, (*raiz)->nome);
+        if (comparar == 0) 
+		return raiz;
+		
+        raiz = (comparar < 0) ? &(*raiz)->esq : &(*raiz)->dir;
+    }
+    return NULL;
 }
 
-void removerID(Tpaciente **raiz, int ID)
+Tpaciente **ppMenor(Tpaciente **raiz)
 {
-	 Tpaciente **pai= NULL;
-    if((*raiz) == NULL)
+    while (*raiz && (*raiz)->esq != NULL)
+        raiz = &(*raiz)->esq;
+
+    return raiz;
+}
+
+
+//Mesmo remove utilizado em aula
+void removerID(Tpaciente **raiz, int ID){	
+    if(*raiz == NULL)
         return;
         
-    pai = buscaPaiID(raiz, ID);
-    Tpaciente *remover_Nome = *pai;
+        Tpaciente **pai=NULL;
+		pai = buscaPaiID(raiz, ID);
+ 		
+ 		if (pai == NULL)
+        return; 
     
-    //1o caso --> remover no folha
     if((*pai)->esq == NULL && (*pai)->dir == NULL)
-	{
-
-    	removerNome(&raiz, remover_Nome->nome);
-        free(remover_Nome);
+    {
+        free(*pai);
         *pai = NULL;
-    }
-    //2o caso --> remover pai com 1 filho apenas
-    else{
-        if(((*pai)->esq != NULL) ^ (*pai)->dir !=NULL)
+        
+        return;
+        
+    }else if(((*pai)->esq != NULL) ^ (*pai)->dir !=NULL)
         {
             if((*pai)->esq != NULL)
-            {
-                Tpaciente *paux=(*pai)->esq; //guardando end. filho
-                free((*pai));
+			{
+                Tpaciente *paux=(*pai)->esq; 
+                free(*pai);
+                *pai = paux;
+				return; 
+				
+            }else if((*pai)->dir != NULL)
+			{
+                Tpaciente *paux=(*pai)->dir;
+                free(*pai);
                 *pai = paux;
             }
-            if((*pai)->dir != NULL)
-            {
-                Tpaciente *paux=(*pai)->dir; //guardando end. filho
-                free((*pai));
-                *pai = paux;
-            }
+            return;
         }
-        //    3o caso --> remover pai com 2 filhos
+      
         else{
-           // if((*pai)->esq != NULL && (*pai)->dir!=NULL)
             {
                 Tpaciente **paux = ppMenor(&(*pai)->dir);
-                //substituiDados
+                printf("\n %d", (*paux)->ID);
+                printf("\nMenor %d %p", (*paux)->ID, paux);
                 (*pai)->ID = (*paux)->ID;
-				strcpy((*pai)->nome, (*paux)->nome);
-				strcpy((*pai)->condicao_medica, (*paux)->condicao_medica);
-                removerNome();
+                strcpy((*pai)->nome, (*paux)->nome);
+                printf("\ntrocou chave...");
+                removerID(paux, (*paux)->ID);
             }
         }
-    }
-}
+	} 
 
-void removerNome(Tpaciente **raiz, char *nome)
-{		 Tpaciente **pai=NULL;
-
+//Mesmo remove utilizado em aula, uma pequena mudança para buscar por nome;
+void removerNome(Tpaciente **raiz, char *nome){
     if((*raiz) == NULL)
         return;
         
-    pai = buscaPaiNome(*raiz, nome);
-    Tpaciente *remover_ID = *pai;
-    
-    //1o caso --> remover no folha
+    Tpaciente **pai=NULL;
+	pai = buscaPaiNome(raiz, nome);
+		 
+	if (pai == NULL)
+       return;
+		 
     if((*pai)->esq == NULL && (*pai)->dir == NULL)
-    {
-        free(remover_ID);
+    {	
+        free(*pai);
         *pai = NULL;
-    
+        return;
     }
-    //2o caso --> remover pai com 1 filho apenas
     else{
         if(((*pai)->esq != NULL) ^ (*pai)->dir !=NULL)
         {
             if((*pai)->esq != NULL)
             {
-                Tpaciente *paux=(*pai)->esq; //guardando end. filho
-                free(remover_ID);
+                Tpaciente *paux=(*pai)->esq;
+                free(*pai);
                 *pai = paux;
-                /*
-                No *paux=(*pai);
-                *pai = (*pai)->esq;
-                free(*paux);
-                */
+             
             }
             if((*pai)->dir != NULL)
-            {
-                Tpaciente *paux=(*pai)->dir; //guardando end. filho
-                free(remover_ID);
+            {	
+                Tpaciente *paux=(*pai)->dir; 
+                free(*pai);
                 *pai = paux;
             }
+            return;
         }
-        //    3o caso --> remover pai com 2 filhos
         else{
-           // if((*pai)->esq != NULL && (*pai)->dir!=NULL)
             {
-                Tpaciente **paux = ppMenor(&remover_ID->dir);
-                strcpy(remover_ID->nome, (*paux)->nome);
-                //substituiDados
-                remover_ID->ID = (*paux)->ID;
-                printf("\ntrocou chave...");
-                strcpy(remover_ID->condicao_medica, (*paux)->condicao_medica);
-                removerNome(&(*pai)->dir, (*paux)->nome);
+                Tpaciente **paux = ppMenor(&(*pai)->dir);
+                (*pai)->ID = (*paux)->ID;
+                strcpy((*pai)->nome, (*paux)->nome);
+                removerNome(paux, (*paux)->nome);
             }
         }
-    }
-			
-	
-	
-	
+    } 
 }
 
-Tpaciente **ppMenor(Tpaciente **raiz)//a entrada ? o lado direito da ?rvore
+void salvar(Tpaciente *raiz, FILE *arq)
 {
-    Tpaciente **pmenor=raiz;
-    while((*pmenor)->esq)
-    {
-        printf("\n%d", (*pmenor)->ID);
-        pmenor = &(*pmenor)->esq;
-    }
-    return pmenor;
+	if(raiz != NULL){
+		//recursividade
+		salvar(raiz->esq, arq);
+		fprintf(arq, "%d %s %d %s\n", raiz->ID, raiz->nome, raiz->idade, raiz->condicao_medica); //Salva no arquivo
+		salvar(raiz->dir, arq);
+	}
 }
